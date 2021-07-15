924556ddb9ad601f892d776c005ee017ca43f993
Adam Morris <aimorris@aimorris.com>
2021-07-15 11:06:38 +1000

    hopefully works!

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,39 @@
-module Main where
+{-# LANGUAGE OverloadedStrings #-}
+
+import Control.Monad (when, void)
+import qualified Data.Text.IO as TIO
+  ( readFile,
+    putStrLn )
+import Discord
+  ( runDiscord,
+    def,
+    restCall,
+    DiscordHandler,
+    RunDiscordOpts(discordToken, discordOnEvent) )
+import Discord.Types
+    ( messageAuthor,
+      messageId,
+      Event(MessageCreate),
+      Message (messageChannel),
+      User (userIsBot) )
+import qualified Discord.Requests as R
+  ( ChannelRequest(CreateReaction) )
 
 main :: IO ()
-main = putStrLn "someFunc"
+main = do
+  token <- TIO.readFile "token.secret"
+  t <- runDiscord $ def {
+    discordToken = token,
+    discordOnEvent = eventHandler
+  }
+  TIO.putStrLn t
+
+eventHandler :: Event -> DiscordHandler ()
+eventHandler e =
+  case e of
+    MessageCreate m -> when (not (fromBot m) && True) $ do
+      void $ restCall (R.CreateReaction (messageChannel m, messageId m) "horseyping")
+    _ -> return ()
+
+fromBot :: Message -> Bool
+fromBot = userIsBot . messageAuthor
